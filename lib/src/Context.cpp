b767c8cf5d24585009dcd19da289ea47408aad09
Roman Arzumanyan <roman.arzum@gmail.com>
2015-11-19 23:23:32 +0300

    Context constructor fixed

--- lib/src/Context.cpp
+++ lib/src/Context.cpp
@@ -9,8 +9,24 @@ Context::Context(
     pfn_notify callback,
     void *user_data)
 {
-    std::vector<Device> vec_devices(devices);
-    Context(vec_devices, properties, callback, user_data);
+    auto i = 0;
+    ret_code ret;
+    cl_device_id *devs = (cl_device_id*)calloc(devices.size(), sizeof(*devs));
+
+    for (auto device : devices)
+        devs[i++] = device.getID();
+
+    context = clCreateContext(
+        properties,
+        devices.size(),
+        devs,
+        callback,
+        user_data,
+        &ret);
+    free(devs);
+
+    if (ret != CL_SUCCESS)
+        throw(SException(ret));
 }
 
 Context::Context(
